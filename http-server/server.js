a99f18a958fba594c3a9984ba026ea170ef36517
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2026-03-22 09:21:37 +0530

    finished the assignment for core Http

--- http-server/server.js
+++ http-server/server.js
@@ -1,1 +1,69 @@
 // start creating server here
+import http from 'http'
+
+let todos = [];
+let currentId = 1;
+let error = {error: "Todo not found"};
+
+http.createServer(function (req, res) {
+
+    if (req.method == 'GET' && (req.url =='/' || req.url.startsWith('/?'))) {
+        res.writeHead(200, { 'Content-Type': 'text' })
+        res.end("Hello World")
+    }
+    
+    if (req.method === 'GET' && req.url ==='/todos') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify(todos));
+    }
+
+    if (req.method === 'POST' && req.url === '/create/todo') {
+        let body = '';
+        req.on('data', chunk => (body += chunk));
+        req.on('end', () => {
+            let data = JSON.parse(body);
+
+            let todo = {
+                id: currentId++,
+                title: data.title,
+                description: data.description
+            };
+            todos.push(todo);
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(todos));
+        });
+    }
+
+    if (req.method === 'GET' && req.url.startsWith('/todo')) {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        const id = Number(url.searchParams.get('id'));
+
+        const todo = todos.find( t => t.id === id);
+
+        if (!todo) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(error));
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(todo));
+        }
+    }
+
+    if (req.method === 'DELETE' && req.url.startsWith('/todo')) {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        const id = Number(url.searchParams.get('id'));
+
+        if (todos.find(t => t.id === id)) {
+            todos = todos.filter(t => t.id !== id);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(todos))
+        } else {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(error))
+        }
+    }
+
+}).listen(3000, () => {
+    console.log("server started at port 3000: http://localhost:3000");
+});
